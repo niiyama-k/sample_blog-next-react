77fed54461da87e45d38edc03667563aa08042ae
niiyama-k <niiyama-k@gitec-cloud.net>
2024-02-18 13:02:25 +0900

    create CLUD API

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -20,7 +20,22 @@ export const GET =async (req: Request, res: NextResponse) => {
     const posts = await prisma.post.findMany();
     return NextResponse.json({massage: "Success", posts}, {status: 200});
   }catch(err){
-    return NextResponse.json({ message: "Error", err}, {status: 500})
+    return NextResponse.json({ message: "Error", err}, {status: 500});
+  }finally{
+    await prisma.$disconnect();
+  }
+}
+  
+  //ブログ投稿用API
+export const POST =async (req: Request, res: NextResponse) => {
+  try{
+    const {title, description} = await req.json();
+
+    await main();
+    const post = await prisma.post.create({data: {title, description}});
+    return NextResponse.json({massage: "Success", post}, {status: 201});
+  }catch(err){
+    return NextResponse.json({ message: "Error", err}, {status: 500});
   }finally{
     await prisma.$disconnect();
   }
